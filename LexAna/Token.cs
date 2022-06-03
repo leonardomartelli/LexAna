a7fb17539b760b981a8b2dc615cae175114a35a4
leonardomartelli <leonardo@martellioliveira.me>
2022-06-02 21:56:53 -0300

    Creating Tokens Enumeration

--- LexAna/Token.cs
+++ LexAna/Token.cs
@@ -1,8 +1,86 @@
-﻿using System;
-namespace LexAna
+﻿namespace LexAna
 {
-	public enum Token
+    public enum Token
 	{
+		LexicalError = -1,
+		EOF,
+		ReservedWord,
+		Identifier,
+		IntegerConstant,
+		FloatingPointConstant,
+
+		//Aritmethical Operators
+		Plus,				// +
+		Minus,				// -
+		Product,			// *
+		Division,			// /
+		Power,				// ˆ
+		Increment,			// ++
+		Decrement,			// --
+		Module,				// %
+
+		//Logical Operators
+		Equals,				// ==
+		NotEquals,			// !=
+		Less,				// <
+		Greater,			// >
+		LessOrEqua,			// <=
+		GreaterOrEqual,		// >=
+		LogicalAnd,			// &&
+		LogicalOr,			// ||
+
+		//Bitwise Operators
+		ShiftLeft,			// <<
+		ShiftRight,			// >>
+		And,				// &
+		Or,					// |
+
+		//Assignment Operators
+		Assign,				// =
+		PlusAssign,			// +=
+		MinusAssign,		// -=
+		ProductAssign,		// *=
+		DivisionAssign,		// /=
+		ModuleAssign,		// %=
+
+		//Language Commands
+		DoWhile,			// do {} while
+		While,				// while
+		If,					// if
+		ElseIf,				// else if
+		Else,				// else
+		Continue,			// continue
+		Break,				// break
+		Return,				// return
+
+		//Switch-case Commands
+		Switch,				// switch
+		Case,				// case
+		Default,			// default
+
+		//Primitive types
+		Int,				// int
+		Char,				// char
+		Double,				// double
+		Float,				// float
+		Struct,				// struct
+		Void,				// void
+
+		//Pointers
+		Accessor,			// ->
+
+		//Characters
+		Underscore,			// _
+		Dot,				// .
+		Comma,				// ,
+		SemiCollon,			// ;
+		Collon,				// :
+		ParenthesisOpen,	// (
+		ParenthesisClose,	// )
+		BracketOpen,		// [
+		BracketClose,		// ]
+		BraceOpen,			// {
+		BraceClose,			// }
 	}
 }
 
