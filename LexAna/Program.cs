5b0857a716104e237e64ef3085568c2c6c2dd26e
leonardomartelli <leonardo@martellioliveira.me>
2022-06-04 18:02:38 -0300

    Asking user for file and folder

--- LexAna/Program.cs
+++ LexAna/Program.cs
@@ -1,8 +1,25 @@
-﻿using LexAna;
+﻿using System.Diagnostics;
+using LexAna;
 
-using var input = new StreamReader("testfile.c");
+var fileName = Debugger.IsAttached ? "testfile.c" : AskUserFileName();
 
-var lexicalAnalyser = new Lexical(input, "results");
+using var input = new StreamReader(fileName);
+
+var folderName = Debugger.IsAttached ? "results" : AskUserFolderName();
+
+var lexicalAnalyser = new Lexical(input, folderName);
 
 lexicalAnalyser.Analyse();
 
+string AskUserFileName() =>
+    AskUser("Digite o caminho para o arquivo de entrada: ");
+
+string AskUserFolderName() =>
+    AskUser("Digite a pasta para salvar os arquivos de resultados: ");
+
+string AskUser(string message)
+{
+    Console.WriteLine(message);
+
+    return Console.ReadLine();
+}
